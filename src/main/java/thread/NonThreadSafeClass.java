6477d2829f73acd6cd7f575c1d5aa67b74586183
Artem <makyanai@yandex.ru>
2023-01-31 22:58:10 +0300

    Commit_4

--- src/main/java/thread/NonThreadSafeClass.java
+++ src/main/java/thread/NonThreadSafeClass.java
@@ -1,5 +1,24 @@
 package thread;
 
-public class NonThreadSafeClass extends Thread {
+public class NonThreadSafeClass {
+    public static void main(String[] args) throws InterruptedException {
+        StringBuilder stringBuilder = new StringBuilder();
+        StringBuffer stringBuffer = new StringBuffer();
 
+        for (int i = 0; i < 2; i++){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int j = 0; j < 50000; j++){
+                        stringBuilder.append("1");
+                        stringBuffer.append("1");
+                    }
+                }
+            }).start();
+        }
+
+        Thread.sleep(1000);
+        System.out.println("StringBuilder: " + stringBuilder.length());
+        System.out.println("StringBuffer: " + stringBuffer.length());
+    }
 }
--- src/main/java/thread/ThreadSafeClass.java
+++
@@ -1,4 +0,0 @@
-package thread;
-
-public class ThreadSafeClass extends Thread {
-}
